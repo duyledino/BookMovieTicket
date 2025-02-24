807ce83ef9a0e35bb90bce3d07e0d798770a0870
duyledinodeptrai300cm <duyle@gmail.com>
2025-02-24 10:30:52 +0700

    update backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,21 +5,6 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-// Use different names to avoid conflicts
-const USER = process.env.USER;
-const PG_PORT = process.env.PG_PORT;
-const PG_DATABASE = process.env.DATABASE;
-const PG_PASSWORD = process.env.PASSWORD;
-
-console.log("🔍 PG_HOST:", PG_HOST);
-console.log("🔍 PG_USER:", USER);
-console.log("🔍 PG_DATABASE:", PG_DATABASE);
-
-// Ensure all required variables are defined
-if (!PG_USER || !PG_HOST || !PG_PORT || !PG_DATABASE || !PG_PASSWORD) {
-    throw new Error("❌ Missing required environment variables for database connection!");
-}
-
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,

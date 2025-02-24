02bbf6bec68cb8e176e2a9c39a217c0325750be8
duyledinodeptrai300cm <duyle@gmail.com>
2025-02-24 10:07:35 +0700

    update backend 3

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,10 @@ app.use("/api/v1/movies",asyncHandle(movieRoutes));
 
 connectDB();
 
+app.use("/",(req,res)=>{
+    res.send("Hello world");
+})
+
 app.listen(PORT,()=>{
     console.log("Listening on http://localhost:5000");
 })

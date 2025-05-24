80993e2e9db6141d2a1178e656e1de5f6972722d
duyledinodeptrai300cm <duyle@gmail.com>
2025-05-24 14:12:23 +0700

    update project to deploy

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server:{ // change port on client
-    port: 8080
+  server: {
+    // change port on client
+    port: 8080,
   },
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  base: "./",
+});

c5fe313fdccb3f9f35b324f057c97b1898d54df5
duyledinodeptrai300cm <duyle@gmail.com>
2025-05-24 14:44:08 +0700

    update

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: {
-    // change port on client
-    port: 8080,
-  },
   plugins: [react(), tailwindcss()],
   base: "./",
 });

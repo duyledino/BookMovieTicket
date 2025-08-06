89ff1e5fec4348b0931784ac0a6b7a06762a2c0d
MyHP <lekhanhduy.bh@gmail.com>
2025-08-06 21:46:04 +0700

    update something 946

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -4,12 +4,12 @@ import env from "dotenv";
 env.config();
 
 const createToken = (user, res) => {
-  const { customer_id, username, customer_name,isAdmin } = user;
+  const { customer_id, username, customer_name, isAdmin } = user;
   const payload = {
     customer_name: customer_name,
     customer_id: customer_id,
     username: username,
-    isAdmin: isAdmin
+    isAdmin: isAdmin,
     // customer_age: customer_age,
   };
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
@@ -17,31 +17,34 @@ const createToken = (user, res) => {
   });
   res.cookie("token", token, {
     httpOnly: true, // Prevents client-side access
-    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-    secure: false,
-    sameSite: "Lax", // Helps with CSRF protection
+    // secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    // sameSite: "Lax", // Helps with CSRF protection
+    secure: process.env.NODE_ENV === "production", // must be true on HTTPS in production
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // "None" allows cross-site cookies
     maxAge: 7 * 24 * 60 * 60 * 1000, // optional: cookie expiry
   });
   return token;
 };
 
 const authenticateUser = (req, res, next) => {
-  console.log("cookies in authenticateUser",req.cookies);
+  console.log("cookies in authenticateUser", req.cookies);
   if (!req.cookies || !req.cookies.token)
     return res.status(203).json({ Message: "No Token" });
   const token = req.cookies.token;
   const decode = jwt.verify(token, process.env.JWT_SECRET);
-  console.log(decode);
-  if (!decode) return res.status(203).json({ Message: "Expire or wrong token" });
+  console.log("decode in authenticateUser;",decode);
+  if (!decode)
+    return res.status(203).json({ Message: "Expire or wrong token" });
   req.user = decode;
   next();
 };
 
 const authenticateAdmin = (req, res, next) => {
-  console.log("user in authenticateAdmin",req.user);
+  console.log("user in authenticateAdmin", req.user);
   const isAdmin = req.user.isAdmin;
   console.log(isAdmin);
-  if (!isAdmin) return res.status(203).json({ Message: "Expire or wrong token" });
+  if (!isAdmin)
+    return res.status(203).json({ Message: "Expire or wrong token" });
   next();
 };
 
